e4f63c7a26ce92700f3a4611a1ce28fd50ce75ef
Your Name <you@example.com>
2024-01-09 15:40:53 +0900

    likes, carts api commit

--- controller/BookController.js
+++ controller/BookController.js
@@ -10,21 +10,23 @@ const allBooks = (req,res)=>{
     //currentPage : 현재 page ex.1, 2, 3 ...
     //offset : limit * (currentPage-1)
     let offset = limit * (currentPage-1);
-    let sql = `SELECT * FROM books`;
+    let sql = `SELECT *, 
+	        (SELECT count(*) FROM likes WHERE liked_book_id = books.id) AS likes 
+            FROM books`;
     let values = [];
     if(category_id && news){
         sql += ` WHERE category_id=? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
-        values.push(category_id);
+        values = [category_id];
     } else if(category_id){
         sql += ` WHERE category_id=?`;
-        values.push(category_id);
+        values = [category_id];
     } else if(news){
         sql += ` WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()`;
     }
     
-    sql +=  `LIMIT ? OFFSET ?`;
+    sql +=  ` LIMIT ? OFFSET ?`;
     values.push(limit, offset);
-    
+
     conn.query(sql, values,
         (err, results)=>{
             if(err){
@@ -39,10 +41,19 @@ const allBooks = (req,res)=>{
 };
 
 const bookDetail = (req,res)=>{
-    let {id} = req.params;
+    let book_id = req.params.id;
+    let {user_id} = req.body;
 
-    let sql = `SELECT * FROM books LEFT JOIN category ON category.id = books.category_id WHERE books.id=?`;
-    conn.query(sql, id,
+    let sql = `SELECT *,
+	                (SELECT count(*) FROM likes WHERE liked_book_id = books.id) AS likes,
+	                (SELECT EXISTS (SELECT * FROM likes WHERE user_id =? AND liked_book_id=?)) AS liked
+                FROM books
+                LEFT JOIN category 
+                ON category.category_id = books.category_id 
+                WHERE books.id=?`;
+    
+    let values = [user_id, book_id, book_id];
+    conn.query(sql, values,
         (err, results)=>{
             if(err){
                 console.log(err);
